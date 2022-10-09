36993ee9a9a8260e5191830c050366703fad98da
xmiao27 <xmiao27@submit2.chtc.wisc.edu>
2022-10-09 11:22:26 -0500

    sync

--- example2/example2.sh
+++ example2/example2.sh
@@ -1,4 +1,5 @@
 source /opt/miniconda/bin/activate
+eval "$(conda shell.bash hook)"
 conda activate example-environment
 
 python3 gpu.py
--- gpu_python_example/gpu_python_example.sh
+++ gpu_python_example/gpu_python_example.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 source /home/xmiao27/miniconda3/bin/activate
 eval "$(conda shell.bash hook)"
 conda activate example
